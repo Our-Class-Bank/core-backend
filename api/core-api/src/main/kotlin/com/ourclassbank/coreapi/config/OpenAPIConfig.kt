3c8c42748c20974675ec0122a73c77bd89169893
Hyune-c <choi8608@gmail.com>
2023-06-09 00:16:43 +0900

    #2 core-backend 초기 세팅 - set openapi config

---
+++ api/core-api/src/main/kotlin/com/ourclassbank/coreapi/config/OpenAPIConfig.kt
@@ -0,0 +1,23 @@
+package com.ourclassbank.coreapi.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
+
+@Configuration
+class OpenAPIConfig {
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("our-class-bank core-api")
+                    .description(
+                        "- ISO 8601 DateTime sample=2020-05-05T10:10:10"
+                    )
+                    .version(LocalDateTime.now().toString())
+            )
+    }
+}
