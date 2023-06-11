c37f0e38b43cfabd850b3a9bc88d2f587c3604d7
Hyune-c <choi8608@gmail.com>
2023-06-11 23:51:35 +0900

    #4 인증/인가 구현 - add security dependency

--- api/core-api/build.gradle.kts
+++ api/core-api/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
     // openapi3
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$openapi3Version")
