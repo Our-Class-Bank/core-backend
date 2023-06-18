8e09a30da90632262f5ce1163093c23d21e0d610
Hyune-c <choi8608@gmail.com>
2023-06-18 15:30:22 +0900

    #6 db 세팅 - set mysql to core-db module

---
+++ db/core-db/build.gradle.kts
@@ -0,0 +1,20 @@
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+dependencies {
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.h2database:h2")
+}
+
+tasks.getByName("bootJar") {
+    enabled = false
+}
+
+tasks.getByName("jar") {
+    enabled = true
+}
