338b2754437db591a5a053b8b9a4253d222e2ffc
Hyune-c <choi8608@gmail.com>
2023-06-18 15:43:21 +0900

    #6 db 세팅 - set jpa plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
+    kotlin("plugin.jpa") version "1.8.21"
 
     id("org.jmailen.kotlinter") version "3.10.0"
     application
@@ -30,6 +31,7 @@ subprojects {
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
 
     apply(plugin = "kotlin")
     apply(plugin = "kotlin-spring")
