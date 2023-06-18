308ba34e80a1c0e11fe918fef73421c716bbe962
Hyune-c <choi8608@gmail.com>
2023-06-18 19:16:13 +0900

    #6 회원 영속화 적용 & 리팩토링 - set p6spy dependency

--- db/core-db/build.gradle.kts
+++ db/core-db/build.gradle.kts
@@ -5,12 +5,15 @@ allOpen {
 }
 
 dependencies {
+    val p6spyVersion: String by project
+
     implementation(project(":domain:model-domain"))
 
+    // jpa + mysql
     api("org.springframework.boot:spring-boot-starter-data-jpa")
-
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:${p6spyVersion}")
 }
 
 tasks.getByName("bootJar") {
