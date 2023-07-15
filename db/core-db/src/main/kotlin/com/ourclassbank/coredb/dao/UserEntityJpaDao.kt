c42400604131c73f41b1cd1e0a97b2dfd48d9445
Hyune-c <choi8608@gmail.com>
2023-07-15 23:52:21 +0900

    #9 용돈계좌 개발 - add 같은 반 회원 조회

--- db/core-db/src/main/kotlin/com/ourclassbank/coredb/dao/UserEntityJpaDao.kt
+++ db/core-db/src/main/kotlin/com/ourclassbank/coredb/dao/UserEntityJpaDao.kt
@@ -2,10 +2,26 @@ package com.ourclassbank.coredb.dao
 
 import com.ourclassbank.coredb.entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserEntityJpaDao : JpaRepository<UserEntity, Long> {
     fun findByLoginId(loginId: String): UserEntity?
     fun existsByLoginId(loginId: String): Boolean
+
+    @Query(
+        value = """
+        select *
+from user
+where school_name = :schoolName
+  and grade = :grade
+  and class_number = :classNumber
+    """, nativeQuery = true
+    )
+    fun findAllByUserClass(
+        schoolName: String,
+        grade: Int,
+        classNumber: Int,
+    ): List<UserEntity>
 }
--- domain/model-domain/src/main/kotlin/com/ourclassbank/modeldomain/user/UserClass.kt
+++ domain/model-domain/src/main/kotlin/com/ourclassbank/modeldomain/user/UserClass.kt
@@ -1,7 +1,6 @@
 package com.ourclassbank.modeldomain.user
 
 data class UserClass(
-    val id: Long? = null,
     val schoolName: String,
     val grade: Int,
     val classNumber: Int,
