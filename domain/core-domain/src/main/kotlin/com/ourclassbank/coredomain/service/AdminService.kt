0a13f22c1f4c2d5e8f6e0011476398da706e8992
Hyune-c <choi8608@gmail.com>
2023-08-26 22:07:41 +0900

    [m6] 회원가입 개발 - 회원가입 service 구현

--- domain/core-domain/src/main/kotlin/com/ourclassbank/coredomain/service/AdminService.kt
+++ domain/core-domain/src/main/kotlin/com/ourclassbank/coredomain/service/AdminService.kt
@@ -1,10 +1,9 @@
 package com.ourclassbank.coredomain.service
 
-import com.ourclassbank.coredomain.support.jwt.JwtTokenProvider
 import com.ourclassbank.coredomain.usecase.AdminUsecase
 import com.ourclassbank.coredomain.usecase.UserCommandUsecase
-import com.ourclassbank.coredomain.usecase.UserQueryUsecase
 import com.ourclassbank.modeldomain.user.RoleType
+import com.ourclassbank.modeldomain.user.User
 import com.ourclassbank.modeldomain.user.UserClass
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -12,10 +11,8 @@ import org.springframework.stereotype.Service
 @Service
 class AdminService(
     private val passwordEncoder: PasswordEncoder,
-    private val jwtTokenProvider: JwtTokenProvider,
 
     private val userCommandUsecase: UserCommandUsecase,
-    private val userQueryUsecase: UserQueryUsecase
 ) : AdminUsecase {
     override fun signup(
         username: String,
@@ -25,20 +22,15 @@ class AdminService(
         roles: List<RoleType>,
         userClass: UserClass
     ) {
-//        userCommandUsecase.create(
-//            User(
-//                username = username,
-//                password = passwordEncoder.encode(password),
-//                name = name,
-//                pocketmoneyAccountNo = "1234-1234-1234-1234",
-//                roles = roles,
-//                userClass = UserClass(
-//                    schoolName = "우리초등학교",
-//                    grade = 3,
-//                    classNumber = 1,
-//                    attendanceNumber = 3
-//                )
-//            )
-//        )
+        userCommandUsecase.create(
+            User(
+                username = username,
+                password = passwordEncoder.encode(password),
+                name = name,
+                pocketmoneyAccountNo = pocketmoneyAccountNo,
+                roles = roles,
+                userClass = userClass
+            )
+        )
     }
 }
