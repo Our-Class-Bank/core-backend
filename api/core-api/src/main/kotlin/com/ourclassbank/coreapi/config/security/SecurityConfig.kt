541d34fe8fba468c89ce72110ee0a684f4d16ec0
Hyune-c <choi8608@gmail.com>
2023-08-26 16:44:41 +0900

    [m6] SecurityConfig 코드 리팩토링

--- api/core-api/src/main/kotlin/com/ourclassbank/coreapi/config/security/SecurityConfig.kt
+++ api/core-api/src/main/kotlin/com/ourclassbank/coreapi/config/security/SecurityConfig.kt
@@ -3,10 +3,6 @@ package com.ourclassbank.coreapi.config.security
 import com.ourclassbank.coreapi.config.security.jwt.JwtAuthenticationFilter
 import com.ourclassbank.coredomain.support.jwt.JwtTokenProvider
 import com.ourclassbank.modeldomain.user.RoleType
-import com.ourclassbank.modeldomain.user.RoleType.ROLE_BANKER
-import com.ourclassbank.modeldomain.user.RoleType.ROLE_CREDIT_EVALUATOR
-import com.ourclassbank.modeldomain.user.RoleType.ROLE_STUDENT
-import com.ourclassbank.modeldomain.user.RoleType.ROLE_TEACHER
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
@@ -16,7 +12,7 @@ import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
@@ -28,6 +24,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 class SecurityConfig(
     private val jwtTokenProvider: JwtTokenProvider
 ) {
+    private val student = RoleType.ROLE_STUDENT.name.removePrefix("ROLE_")
+    private val banker = RoleType.ROLE_BANKER.name.removePrefix("ROLE_")
+    private val teacher = RoleType.ROLE_TEACHER.name.removePrefix("ROLE_")
+    private val creditEvaluator = RoleType.ROLE_CREDIT_EVALUATOR.name.removePrefix("ROLE_")
+
     @Bean
     fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
         return authenticationConfiguration.authenticationManager
@@ -35,48 +36,44 @@ class SecurityConfig(
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .authorizeHttpRequests { request ->
-                request
-                    // swagger, actuator
-                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
-                    .requestMatchers("/actuator/**").permitAll()
+        http.authorizeHttpRequests { request ->
+            request
+                // swagger, actuator
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
 
-                    // 인증, 인가
-                    .requestMatchers("/api/v1/auth/**").permitAll()
+                // 인증, 인가
+                .requestMatchers("/api/v1/auth/**").permitAll()
 
-                    // 내 정보
-                    .requestMatchers("/api/v1/my/**").hasAnyRole(ROLE_STUDENT.toSecurityRole())
+                // 내 정보
+                .requestMatchers("/api/v1/my/**").hasAnyRole(student)
 
-                    // 같은 반
-                    .requestMatchers("/api/v1/same-class/**").hasAnyRole(
-                        ROLE_STUDENT.toSecurityRole(),
-                        ROLE_TEACHER.toSecurityRole()
-                    )
+                // 같은 반
+                .requestMatchers("/api/v1/same-class/**").hasAnyRole(student, teacher)
 
-                    // 신용평가
-                    .requestMatchers("/api/v1/credit-evaluation/**").hasRole(ROLE_CREDIT_EVALUATOR.toSecurityRole())
+                // 신용평가
+                .requestMatchers("/api/v1/credit-evaluation/**").hasRole(creditEvaluator)
 
-                    // 용돈 계좌
-                    .requestMatchers("/api/v1/account/pocketmoney/**").hasRole(ROLE_BANKER.toSecurityRole())
+                // 용돈 계좌
+                .requestMatchers("/api/v1/account/pocketmoney/**").hasRole(banker)
 
-                    // 선생님에게는 전체 권한
-                    .requestMatchers("/api/v1/**").hasRole(ROLE_TEACHER.toSecurityRole())
+                // 선생님에게는 전체 권한
+                .requestMatchers("/api/v1/**").hasRole(teacher)
 
-                    // test api
-                    .requestMatchers("/test/api/v1/auth/student").hasRole(ROLE_STUDENT.toSecurityRole())
-                    .requestMatchers("/test/api/v1/auth/banker").hasRole(ROLE_BANKER.toSecurityRole())
-                    .requestMatchers("/test/api/v1/auth/teacher").hasRole(ROLE_TEACHER.toSecurityRole())
-                    .requestMatchers("/test/**").permitAll()
+                // test api
+                .requestMatchers("/test/api/v1/auth/student").hasRole(student)
+                .requestMatchers("/test/api/v1/auth/banker").hasRole(banker)
+                .requestMatchers("/test/api/v1/auth/teacher").hasRole(teacher)
+                .requestMatchers("/test/**").permitAll()
 
-                    // util api
-                    .requestMatchers("/util/**").permitAll()
+                // util api
+                .requestMatchers("/util/**").permitAll()
 
-                    .anyRequest().authenticated()
-            }
+                .anyRequest().authenticated()
+        }
             .csrf { it.disable() }
             .cors { it.disable() }
-            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .sessionManagement { it.sessionCreationPolicy(STATELESS) }
             .addFilterBefore(
                 JwtAuthenticationFilter(jwtTokenProvider),
                 UsernamePasswordAuthenticationFilter::class.java
@@ -85,13 +82,6 @@ class SecurityConfig(
             .exceptionHandling { it.accessDeniedHandler(CustomAccessDeniedHandler()) }
 
         return http.build()
-    }
-
-    /**
-     * security 가 role 을 해석하기 위해서는 ROLE_ prefix 가 없어야 합니다.
-     */
-    private fun RoleType.toSecurityRole(): String {
-        return this.name.removePrefix("ROLE_")
     }
 
     class CustomAuthenticationEntryPoint : AuthenticationEntryPoint {
