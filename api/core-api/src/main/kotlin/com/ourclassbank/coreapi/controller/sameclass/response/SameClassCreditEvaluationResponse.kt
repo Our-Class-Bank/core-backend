b7ef67f4ab872b3d5d73c59742f7522c885b1213
Hyune-c <choi8608@gmail.com>
2023-09-13 20:45:41 +0900

    [m6] 같은 반의 현재 신용평가 점수 조회 api response 의 user 정보를 username 으로 변경

--- api/core-api/src/main/kotlin/com/ourclassbank/coreapi/controller/sameclass/response/SameClassCreditEvaluationResponse.kt
+++ api/core-api/src/main/kotlin/com/ourclassbank/coreapi/controller/sameclass/response/SameClassCreditEvaluationResponse.kt
@@ -1,7 +1,6 @@
 package com.ourclassbank.coreapi.controller.sameclass.response
 
 data class SameClassCreditEvaluationResponse(
-    val attendanceNumber: Int,
-    val studentName: String,
+    val username: String,
     val score: Int
 )
