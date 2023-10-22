61cb211658162add568b42d5ee9493072e7da7ba
Hyune-c <choi8608@gmail.com>
2023-10-22 18:22:01 +0900

    데모회원 로그인 api 구현

--- api/core-api/src/main/kotlin/com/ourclassbank/coreapi/controller/auth/AuthController.kt
+++ api/core-api/src/main/kotlin/com/ourclassbank/coreapi/controller/auth/AuthController.kt
@@ -1,5 +1,6 @@
 package com.ourclassbank.coreapi.controller.auth
 
+import com.ourclassbank.coreapi.controller.auth.request.DemoUserSigninRequest
 import com.ourclassbank.coreapi.controller.auth.request.UserSigninRequest
 import com.ourclassbank.coreapi.controller.auth.response.UserPasswordChangeRequest
 import com.ourclassbank.coreapi.controller.auth.response.UserSigninResponse
@@ -19,6 +20,12 @@ class AuthController(
     @PostMapping("/api/v1/auth/signin")
     fun signin(@RequestBody request: UserSigninRequest): UserSigninResponse {
         return UserSigninResponse(authUsecase.signin(request.username, request.password))
+    }
+
+    @Operation(summary = "데모 회원 로그인")
+    @PostMapping("/api/v1/auth/signin/demo")
+    fun demoSignin(@RequestBody request: DemoUserSigninRequest): UserSigninResponse {
+        return UserSigninResponse(authUsecase.demoSignin(request.demoUserType.username))
     }
 
     @Operation(summary = "회원 비밀번호 변경")
--- domain/core-domain/src/main/kotlin/com/ourclassbank/coredomain/usecase/AuthUsecase.kt
+++ domain/core-domain/src/main/kotlin/com/ourclassbank/coredomain/usecase/AuthUsecase.kt
@@ -2,6 +2,7 @@ package com.ourclassbank.coredomain.usecase
 
 interface AuthUsecase {
     fun signin(username: String, password: String): String
+    fun demoSignin(username: String): String
     fun passwordReset(username: String, name: String)
     fun passwordChange(username: String, name: String, newPassword: String)
 }
